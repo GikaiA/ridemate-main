37eb1bf7e7a2b40bb1d7e75def9c20d56579c028
JerushaStewart <136763972+JerushaStewart@users.noreply.github.com>
2023-08-03 17:30:30 -0400

    Update OfferRide.js

--- ridemate/src/components/OfferRide/OfferRide.js
+++ ridemate/src/components/OfferRide/OfferRide.js
@@ -1,11 +1,50 @@
 import React from 'react'
 
-function OfferRide() {
+
+const OfferRide = () => {
+  const driverData = {
+    name: "John Doe",
+    status: "Active",
+    joinLink: "#",
+  };
+
+
   return (
-    <div>
-      
+ <div className="container">
+      <div className="rectangle">
+        <div className="info">
+          <div className="profile-image"></div>
+          <div className="driver-info">
+            <p>{driverData.name}</p>
+            <p>Status: {driverData.status}</p>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="status-btn">{driverData.status}</button>
+          <a href={driverData.joinLink} className="join-btn">
+            Join
+          </a>
+        </div>
+      </div>
+
+      {/* Duplicate the rectangle */}
+      <div className="rectangle">
+        <div className="info">
+          <div className="profile-image"></div>
+          <div className="driver-info">
+            <p>{driverData.name}</p>
+            <p>Status: {driverData.status}</p>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="status-btn">{driverData.status}</button>
+          <a href={driverData.joinLink} className="join-btn">
+            Join
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+ };
 
 export default OfferRide
